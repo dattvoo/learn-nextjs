35d1bcaca5e3cdaef85becd75aabc4ba6abc9153
Dat Vo <datvodat288@gmail.com>
2025-08-20 22:13:03 +0700

    Implement UI for QR code

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -7,18 +7,36 @@ import { getTableLink } from "@/lib/utils"
 export default function QRcodeTable({ tableNumber, token, width = 250 }: { tableNumber: number, token: string, width?: number }) {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
+
     useEffect(() => {
-        const canvas = canvasRef.current
-        QRCode.toCanvas(canvas, getTableLink({
+        // Hien tai : Thu vien QRcode se ve len cai the canvas
+        // Bay gio: Ta se tao mot canvas de thu vien QRcode se gi de len do
+        // Chung ta se them text vao the canvas that
+        // CUoi chung ta se dua the canvas cua QRcode vao canvas that
+        const canvas = canvasRef.current!
+        canvas.height = width + 70
+        canvas.width = width
+        const canvasContext = canvas.getContext('2d')!
+        canvasContext.fillStyle = '#fff'
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+        canvasContext.font = '20px Arial'
+        canvasContext.textAlign = 'center'
+        canvasContext.fillStyle = '#000'
+        canvasContext.fillText(`Bàn số ${tableNumber}`, canvas.width / 2, canvas.width + 20)
+        canvasContext.fillText(`Quét QR để gọi món`, canvas.width / 2, canvas.width + 50)
+
+        // Here we create a virtalDom in order to overwrite canvas we created before
+        const virtalCanvas = document.createElement('canvas')
+        QRCode.toCanvas(virtalCanvas, getTableLink({
             token,
             tableNumber,
         }), function (error) {
             if (error) {
                 console.log(error);
             }
-            console.log('success');
+            canvasContext.drawImage(virtalCanvas, 0, 0, width, width)
         })
-    }, [tableNumber, token])
+    }, [tableNumber, token, width])
 
     return <canvas ref={canvasRef} width={width} />
 }
